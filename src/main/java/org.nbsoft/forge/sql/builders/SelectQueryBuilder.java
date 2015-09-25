82b6ca791fe28ec66f73e86590a4261f7799c33f
Nils Berlijn <nberlijn@outlook.com>
2015-09-25 21:10:45 +0200

    Names updated into logical names for the ArrayLists

--- src/main/java/org.nbsoft/forge/sql/builders/SelectQueryBuilder.java
+++ src/main/java/org.nbsoft/forge/sql/builders/SelectQueryBuilder.java
@@ -20,16 +20,17 @@ import org.nbsoft.forge.sql.Query;
 import org.nbsoft.forge.sql.syntax.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SelectQueryBuilder implements SelectQuery {
 
     private String select;
     private String from;
 
-    private ArrayList<String> conjunction = new ArrayList<>();
-    private ArrayList<String> where = new ArrayList<>();
-    private ArrayList<String> operator= new ArrayList<>();
-    private ArrayList<Object> what = new ArrayList<>();
+    private List<String> conjunctionList = new ArrayList<>();
+    private List<String> whereList = new ArrayList<>();
+    private List<String> operatorList = new ArrayList<>();
+    private List<Object> whatList = new ArrayList<>();
 
     @Override
     public From select(String select) {
@@ -47,96 +48,96 @@ public class SelectQueryBuilder implements SelectQuery {
 
     @Override
     public Operator where(String where) {
-        this.conjunction.add("WHERE");
-        this.where.add(where);
+        conjunctionList.add("WHERE");
+        whereList.add(where);
 
         return this;
     }
 
     @Override
     public Conjunction equal(Object equal) {
-        operator.add("=");
-        what.add(equal);
+        operatorList.add("=");
+        whatList.add(equal);
 
         return this;
     }
 
     @Override
     public Conjunction notEqual(Object notEqual) {
-        operator.add("<>");
-        what.add(notEqual);
+        operatorList.add("<>");
+        whatList.add(notEqual);
 
         return this;
     }
 
     @Override
     public Conjunction greaterThan(Object greaterThan) {
-        operator.add(">");
-        what.add(greaterThan);
+        operatorList.add(">");
+        whatList.add(greaterThan);
 
         return this;
     }
 
     @Override
     public Conjunction lessThan(Object lessThan) {
-        operator.add("<");
-        what.add(lessThan);
+        operatorList.add("<");
+        whatList.add(lessThan);
 
         return this;
     }
 
     @Override
     public Conjunction greaterThanOrEqual(Object greaterThanOrEqual) {
-        operator.add(">=");
-        what.add(greaterThanOrEqual);
+        operatorList.add(">=");
+        whatList.add(greaterThanOrEqual);
 
         return this;
     }
 
     @Override
     public Conjunction lessThanOrEqual(Object greaterThanOrEqual) {
-        operator.add("<=");
-        what.add(greaterThanOrEqual);
+        operatorList.add("<=");
+        whatList.add(greaterThanOrEqual);
 
         return this;
     }
 
     @Override
     public Conjunction between(Object between) {
-        operator.add("BETWEEN");
-        what.add(between);
+        operatorList.add("BETWEEN");
+        whatList.add(between);
 
         return this;
     }
 
     @Override
     public Conjunction like(Object like) {
-        operator.add("LIKE");
-        what.add(like);
+        operatorList.add("LIKE");
+        whatList.add(like);
 
         return this;
     }
 
     @Override
     public Conjunction in(Object in) {
-        operator.add("IN");
-        what.add(in);
+        operatorList.add("IN");
+        whatList.add(in);
 
         return this;
     }
 
     @Override
     public Operator and(String and) {
-        this.conjunction.add("AND");
-        this.where.add(and);
+        operatorList.add("AND");
+        whereList.add(and);
 
         return this;
     }
 
     @Override
     public Operator or(String or) {
-        this.conjunction.add("OR");
-        this.where.add(or);
+        operatorList.add("OR");
+        whereList.add(or);
 
         return this;
     }
@@ -146,9 +147,9 @@ public class SelectQueryBuilder implements SelectQuery {
         String query = "SELECT " + select + "\n";
         query += "FROM " + from;
 
-        if (!conjunction.isEmpty()) {
-            for (int i = 0; i < where.size(); i++) {
-                query += "\n" + conjunction.get(i) + " " + where.get(i) + " " + operator.get(i) + " " + what.get(i);
+        if (!conjunctionList.isEmpty()) {
+            for (int i = 0; i < conjunctionList.size(); i++) {
+                query += "\n" + conjunctionList.get(i) + " " + whereList.get(i) + " " + operatorList.get(i) + " " + whatList.get(i);
             }
         }
 
