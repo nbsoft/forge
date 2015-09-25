14a41a35d8575593bb881a5aeb79e9bd82e97845
Nils Berlijn <nberlijn@outlook.com>
2015-09-25 22:27:40 +0200

    Enter added between the license and package

--- src/main/java/org.nbsoft/forge/sql/syntax/orders/DESC.java
+++ src/main/java/org.nbsoft/forge/sql/syntax/orders/DESC.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.nbsoft.forge.sql.syntax.orders;
 
 import org.nbsoft.forge.sql.syntax.Build;
